6cd783e4b31ef9f00d2852a7f706a5916c9e488a
Krishan Koenig <krishan.koenig@googlemail.com>
2020-04-05 14:52:17 +1200

    WIP

--- src/Traits/InteractsWithRelationMethods.php
+++ src/Traits/InteractsWithRelationMethods.php
@@ -2,20 +2,20 @@
 
 namespace Naoray\EloquentModelAnalyzer\Traits;
 
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Relations\HasManyThrough;
-use Illuminate\Database\Eloquent\Relations\HasOne;
-use Illuminate\Database\Eloquent\Relations\HasOneThrough;
-use Illuminate\Database\Eloquent\Relations\MorphOne;
-use Illuminate\Database\Eloquent\Relations\MorphTo;
-use Illuminate\Database\Eloquent\Relations\MorphToMany;
+use SplFileObject;
+use ReflectionType;
+use ReflectionMethod;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
-use ReflectionMethod;
-use ReflectionType;
-use SplFileObject;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasOneThrough;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 
 trait InteractsWithRelationMethods
 {
@@ -48,7 +48,7 @@ trait InteractsWithRelationMethods
 
     protected function hasReturnTypeInDoc(ReflectionMethod $method): bool
     {
-        return Str::of($method->getDocComment())->contains('@return');
+        return Str::contains($method->getDocComment(), '@return');
     }
 
     /**
@@ -57,7 +57,7 @@ trait InteractsWithRelationMethods
      */
     protected function hasRelationTypeInDoc(ReflectionMethod $method): bool
     {
-        return (bool) $this->extractReturnTypeFromDocs(
+        return (bool)$this->extractReturnTypeFromDocs(
             $method->getDocComment()
         );
     }
@@ -69,7 +69,7 @@ trait InteractsWithRelationMethods
     protected function extractReturnTypeFromDocs(string $docComment)
     {
         return Arr::first(static::getRelationTypes(), function ($type) use ($docComment) {
-            return Str::contains($docComment, '@return '.class_basename($type))
+            return Str::contains($docComment, '@return ' . class_basename($type))
                 || Str::contains($docComment, "@return \\$type");
         });
     }
